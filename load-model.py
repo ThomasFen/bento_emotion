201216793d75af7e74b5791bc165332bc0522660
Thomas Fengler <fenglerthomas@icloud.com>
2023-02-25 19:09:50 +0100

    Enabled emotion model adaptive batching

--- load-model.py
+++ load-model.py
@@ -1,32 +1,38 @@
-## LOADS THE EMOTION MODEL
+import onnx, os, bentoml, torch
+from blazeface import BlazeFace
 
-# import onnx, os, bentoml
+## LOADS THE EMOTION MODEL 
 
-# modelPath = os.path.join(os.path.expanduser('~'), 'dev', 'bento', 'emotion.onnx')
+modelPath = os.path.join(os.path.expanduser('~'), 'dev', 'bento_emotion', 'emotion.onnx')
 
-# onnx_model = onnx.load(modelPath)
+onnx_model = onnx.load(modelPath)
 
-# bentoml.onnx.save_model("emotion", onnx_model)
+bentoml.onnx.save_model("emotion", onnx_model, signatures={   # model signatures for runner inference
+                                "run": {
+                                "batchable": True,
+                                    }
+                            }
+                         )
 
 ## LOADS THE BLAZEFACE MODEL
 
-# import torch, bentoml, os
-# from blazeface import BlazeFace
+modelPath = os.path.join(os.path.expanduser('~'), 'dev', 'bento_emotion', 'blazefaceback.pth')
+torch_model_back = BlazeFace(back_model=True)
+torch_model_back.load_weights(modelPath)
 
-# modelPath = os.path.join(os.path.expanduser('~'), 'dev', 'bento', 'blazefaceback.pth')
+bentoml.pytorch.save_model( "blazeface_back",
+                            torch_model_back, 
+                            signatures={   # model signatures for runner inference
+                                "__call__": {
+                                "batchable": True,
+                                    }
+                            }
+)
 
 
-# torch_model_back = BlazeFace(back_model=True)
-# torch_model_back.load_weights(modelPath)
+# ## LOADS THE Blazeface ONNX model (useless for now because only 1 output supported by bentoml)
 
-# bentoml.pytorch.save_model( "blazeface_back", torch_model_back)
-
-
-## LOADS THE Blazeface ONNX model (useless for now because only 1 output supported by bentoml)
-
-# import onnx, os, bentoml
-
-# modelPath = os.path.join(os.path.expanduser('~'), 'dev', 'bento', 'blazeface_back_converted.onnx')
+# modelPath = os.path.join(os.path.expanduser('~'), 'dev', 'bento_emotion', 'blazeface_back_converted.onnx')
 
 # onnx_model = onnx.load(modelPath)
 
